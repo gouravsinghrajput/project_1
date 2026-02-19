7ab17cd002408934f4bdde50ec33f3e5574f49cc
gouravsinghrajput <gsrajput24sept@gmail.com>
2026-02-19 22:15:32 +0530

    Added the libraries.

--- project_1.py
+++ project_1.py
@@ -1,1 +1,7 @@
-
+#----------Libraries-----------------------------
+import cv2 as cv 
+import mediapipe as mp 
+import numpy as np 
+import math 
+import pygame 
+#------------------------------------------------
