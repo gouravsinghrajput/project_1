1dd3be840405c7b4b062bb9c1c9c1d22d4a3a951
gouravsinghrajput <gsrajput24sept@gmail.com>
2026-02-25 21:07:22 +0530

    addded the logic for index finger up, and it's coordinate.

--- hand_module.py
+++ hand_module.py
@@ -35,6 +35,25 @@ while True:
                 frame, hand_landmarks, 
                 # mp_hands.HAND_CONNECTIONS   #enable this to show lines between the points on the hands.
             )
+
+            landmark_list = []
+            for landmark_id, landmark in enumerate(hand_landmarks.landmark):
+                h, w, c = frame.shape 
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+                landmark_list.append((landmark_id, (cx, cy)))
+            if landmark_list:
+                landmark_id, (cx, cy) = landmark_list[8]
+                #the below line is to show the coordinates if index finger tip.
+                # cv.putText(frame, f'({cx}, {cy})', (cx, cy + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+            #------logic for index finger up --------------
+            if ((cy < landmark_list[5][1][1]) and
+                ((cy < landmark_list[12][1][1])) and 
+                ((cy < landmark_list[16][1][1])) and 
+                ((cy < landmark_list[20][1][1]))):
+                    cv.putText(frame, "Index Finger is up", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            #----------------------------------------------
+
+
     # color = cv.cvtColor(frame, cv.COLOR_BGR2BGR555)
     cv.imshow('frame', frame)
 
