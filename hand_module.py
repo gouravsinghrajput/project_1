974fb1838e63106c15b84ad0b3e0e168d02d766c
gouravsinghrajput <gsrajput24sept@gmail.com>
2026-02-26 21:59:22 +0530

    added the logic for middle finger.

--- hand_module.py
+++ hand_module.py
@@ -41,17 +41,32 @@ while True:
                 h, w, c = frame.shape 
                 cx, cy = int(landmark.x * w), int(landmark.y * h)
                 landmark_list.append((landmark_id, (cx, cy)))
+            
+            
+            #------logic for index finger up --------------
             if landmark_list:
                 landmark_id, (cx, cy) = landmark_list[8]
                 #the below line is to show the coordinates if index finger tip.
                 # cv.putText(frame, f'({cx}, {cy})', (cx, cy + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
-            #------logic for index finger up --------------
             if ((cy < landmark_list[5][1][1]) and
                 ((cy < landmark_list[12][1][1])) and 
                 ((cy < landmark_list[16][1][1])) and 
                 ((cy < landmark_list[20][1][1]))):
                     cv.putText(frame, "Index Finger is up", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             #----------------------------------------------
+
+
+            #------logic for middle finger up ---------------
+            if landmark_list:
+                landmark_id, (cx, cy) = landmark_list[10]
+                # cv.putText(frame, f'({cx}, {cy})', (cx, cy + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+            if ((cy < landmark_list[4][1][1]) and
+                ((cy < landmark_list[8][1][1])) and not 
+                ((cy < landmark_list[11][1][1])) and 
+                ((cy < landmark_list[16][1][1])) and 
+                ((cy < landmark_list[20][1][1]))):
+                    cv.putText(frame, "Middle Finger is up", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            #-----------------------------------------------        
 
 
     # color = cv.cvtColor(frame, cv.COLOR_BGR2BGR555)
