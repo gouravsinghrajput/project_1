e7b1ad3a49627946ae6315ad80c6281255301597
gouravsinghrajput <gsrajput24sept@gmail.com>
2026-02-26 22:22:48 +0530

    mediapipe conditions for hands in main file

--- project_1.py
+++ project_1.py
@@ -18,6 +18,16 @@ if not cap.isOpened():
 # cv.resizeWindow('frame', 1000, 900)
 #----------------------------------------------------------------------------------
 
+#-----------for hands------------------------
+mp_hands = mp.solutions.hands 
+mp_draw = mp.solutions.drawing_utils
+hands = mp_hands.Hands(
+    max_num_hands = 4,
+    min_detection_confidence = 0.7, 
+    min_tracking_confidence = 0.6
+)
+#-------------------------------------------
+ 
 
 while True:
     ret, frame = cap.read()
