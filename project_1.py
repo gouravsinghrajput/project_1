14246d9ee58121a7b69fa5918807b571ef99ade3
gouravsinghrajput <gsrajput24sept@gmail.com>
2026-02-26 22:23:56 +0530

    started the function for finger detection in the main file.

--- project_1.py
+++ project_1.py
@@ -27,6 +27,51 @@ hands = mp_hands.Hands(
     min_tracking_confidence = 0.6
 )
 #-------------------------------------------
+
+
+#----------- function for fingers ----------------
+def fingers_detecton():
+    results = hands.process(frame)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(
+                frame, hand_landmarks, 
+                # mp_hands.HAND_CONNECTIONS   #enable this to show lines between the points on the hands.
+            )
+
+            landmark_list = []
+            for landmark_id, landmark in enumerate(hand_landmarks.landmark):
+                h, w, c = frame.shape 
+                cx, cy = int(landmark.x * w), int(landmark.y * h)
+                landmark_list.append((landmark_id, (cx, cy)))
+            
+            
+            #------logic for index finger up --------------
+            if landmark_list:
+                landmark_id, (cx, cy) = landmark_list[8]
+                #the below line is to show the coordinates if index finger tip.
+                # cv.putText(frame, f'({cx}, {cy})', (cx, cy + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+            if ((cy < landmark_list[5][1][1]) and
+                ((cy < landmark_list[12][1][1])) and 
+                ((cy < landmark_list[16][1][1])) and 
+                ((cy < landmark_list[20][1][1]))):
+                    cv.putText(frame, "Index Finger is up", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            #----------------------------------------------
+
+
+            #------logic for middle finger up ---------------
+            if landmark_list:
+                landmark_id, (cx, cy) = landmark_list[10]
+                # cv.putText(frame, f'({cx}, {cy})', (cx, cy + 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 1)
+            if ((cy < landmark_list[4][1][1]) and
+                ((cy < landmark_list[8][1][1])) and not 
+                ((cy < landmark_list[11][1][1])) and 
+                ((cy < landmark_list[16][1][1])) and 
+                ((cy < landmark_list[20][1][1]))):
+                    cv.putText(frame, "Middle Finger is up", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            #-----------------------------------------------
+            pass
  
 
 while True:
